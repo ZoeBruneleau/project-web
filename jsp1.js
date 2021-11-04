3a8dda4237dd7fe5b6ac2a7aa107ee634da45fd9
ZoeBruneleau <92059910+ZoeBruneleau@users.noreply.github.com>
2021-11-04 13:35:29 +0100

    jsp1+cssp1

---
+++ jsp1.js
@@ -0,0 +1,16 @@
+function Over() {
+  document.getElementById("h1").style.color = "grey";
+}
+
+function Out() {
+  document.getElementById("h1").style.color = "black";
+}
+
+function Ov() {
+    document.getElementById("a").style.backgroundColor = "black";
+
+  }
+  
+  function Ou() {
+    document.getElementById("a").style.backgroundColor= "gray";
+  }
